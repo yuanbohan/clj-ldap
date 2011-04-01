3d55c630f4f5b0d887c05b97978fb4b3e10c4475
Paul Dorman <paul.dorman@solnetsolutions.co.nz>
2011-04-01 15:45:22 +1300

    Version bumped to 0.0.7.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.pntblnk/clj-ldap "0.0.5"
+(defproject org.clojars.pntblnk/clj-ldap "0.0.7"
   :description "Clojure ldap client (development fork of alienscience's clj-ldap)."
   :url "https://github.com/pauldorman/clj-ldap"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.unboundid/unboundid-ldapsdk "2.0.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+                 [com.unboundid/unboundid-ldapsdk "2.1.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]
                      [jline "0.9.94"]
                      [org.apache.directory.server/apacheds-all "1.5.5"]
                      [org.slf4j/slf4j-simple "1.5.6"]
