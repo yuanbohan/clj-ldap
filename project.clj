9d03b903768213a44a4301bac7f4637ab570e612
William Roe <willroe@gmail.com>
2012-01-06 23:21:04 +0000

    Remove clojars repo from project.clj
    Leiningen has this repo by default

--- project.clj
+++ project.clj
@@ -10,7 +10,6 @@
                      [org.slf4j/slf4j-simple "1.5.6"]
                      [clj-file-utils "0.2.1"]
                      [lein-clojars "0.6.0"]]
-  :repositories [["clojars" "http://clojars.org/repo"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
